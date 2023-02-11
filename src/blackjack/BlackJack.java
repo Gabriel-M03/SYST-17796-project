301b87604b01b3b7ce229f20773012cee373271e
Gabriel-M03 <montemga@sheridancollege.ca>
2023-02-11 13:03:52 -0500

    SYST 17796 BlackJack game

---
+++ src/blackjack/BlackJack.java
@@ -0,0 +1,13 @@
+package blackjack;
+
+public class BlackJack 
+{
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        
+    }
+
+}
