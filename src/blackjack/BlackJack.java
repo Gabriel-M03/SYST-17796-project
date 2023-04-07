4ff21fac97d08c89fecae9991e88caeb4e0ff824
Gabriel-M03 <montemga@sheridancollege.ca>
2023-04-07 14:43:31 -0400

    New updates: - Players and dealer given cards - Card fail safe added

--- src/blackjack/BlackJack.java
+++ src/blackjack/BlackJack.java
@@ -10,6 +10,7 @@ public class BlackJack
     public static void main(String[] args) 
     {
         ArrayList<Player> players = new ArrayList<>();
+        ArrayList<Card> cards = new ArrayList<>();
         System.out.println("Welcome to Blackjack! Let's start!");
         System.out.println("How many Players are playing?");
         Scanner input = new Scanner(System.in);
@@ -23,7 +24,7 @@ public class BlackJack
             }
         }
        while(noOfPlayers >= 5);
-        for(int i=0;i<noOfPlayers;i++)
+        for(int i=0;i<noOfPlayers;i++)//Base settings for players 
         {
             System.out.println("Enter name of Player " + (i+1));
             String name = input.next();
@@ -36,11 +37,32 @@ public class BlackJack
                 System.out.println("You cannot bet higher than your bank amount, try again");
                 bet = input.nextInt();
             }
-            for(int a = 0; a < 2; a++)
+            //Players given cards
+            System.out.println(players.get(i).getName() + " is given: ");
+            Card[] cardHand = GroupOfCards.generateHand(2);
+            if(cardHand == cardHand)
             {
-                GroupOfCards cards = GroupOfCards.generateHand(a);
+                System.out.println("Identical cards given - reshuffling");
+                for (Card card : cardHand) 
+                {
+                    System.out.println(card.getValue() + " of " + card.getSuit());
+                }
             }
-            
+            else
+            {   
+                System.out.println("No duplicates - continuing");
+                for (Card card : cardHand) 
+                {
+                    System.out.println(card.getValue() + " of " + card.getSuit());
+                }  
+            }
+        }
+        //Dealer is given card
+        System.out.println("\nDealer is given:");
+        Card [] cardHand = GroupOfCards.generateHand(1);
+        for (Card card : cardHand) 
+        {
+            System.out.println(card.getValue() + " of " + card.getSuit());
         }
     }
 }
--- src/blackjack/GroupOfCards.java
+++ src/blackjack/GroupOfCards.java
@@ -13,7 +13,7 @@ public class GroupOfCards
 {
 
     //The group of cards, stored in an ArrayList
-    private ArrayList<Card> cards;
+    protected ArrayList<Card> cards;
     private int size = 2;//the size of the grouping
 
     public GroupOfCards(int size) 
