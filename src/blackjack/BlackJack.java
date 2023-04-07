a4cf997c0096e1423a69d565603d2919bdc71816
Gabriel-M03 <montemga@sheridancollege.ca>
2023-04-07 14:03:11 -0400

    Project with updated UML & New changes

--- src/blackjack/BlackJack.java
+++ src/blackjack/BlackJack.java
@@ -36,8 +36,10 @@ public class BlackJack
                 System.out.println("You cannot bet higher than your bank amount, try again");
                 bet = input.nextInt();
             }
-            
-            
-        }
-    }
-}
+            for(Player p : players)
+            {
+                GroupOfCards.generateHand(i);
+            }           
+        }    
+    }//End of Main method
+}//End of main class
